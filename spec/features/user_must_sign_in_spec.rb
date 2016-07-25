44c641a536a41024e5f89306c4b77325f12a27cb
Chad Ellison <chad.ellison0123@gmail.com>
2016-07-25 09:21:59 -0600

    Initial commit

---
+++ spec/features/user_must_sign_in_spec.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+
+RSpec.feature "user must sign in" do
+  scenario "user is prompted to sign up or sign in" do
+    
+  end
+end
+#
+# As an unauthenticated user, when I visit the root of the application,
+# I should be redirected to a page which prompts me to "Log In or Sign Up".
