5bcdd04d8355f9fc95ca236f1e044c60f0a319c0
Chad Ellison <chad.ellison0123@gmail.com>
2016-07-26 22:17:07 -0600

    Add user association Improve login / sign up experience

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,4 +10,11 @@ RSpec.describe User, type: :model do
     User.create(username: "jones", password: "123", email: "abc")
     expect(User.create(username: "Martha", password: "123", email: "abc")).not_to be_valid
   end
+
+  it "should have many links" do
+    user = User.create(username: "frank", password: "123", email: "j@gmail.com")
+    link = Link.create(title: "hearts", url: "https://www.google.com/?gws_rd=ssl")
+    user.links << link
+    expect(user.links).to eq [link]
+  end
 end
