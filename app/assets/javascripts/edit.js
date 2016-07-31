553fc96e4e6c5fde838669cdd1c725bdbd6db5f3
Chad Ellison <chad.ellison0123@gmail.com>
2016-07-31 00:58:53 -0600

    Style buttons Add sad paths to login and account creation specs

--- app/assets/javascripts/edit.js
+++ app/assets/javascripts/edit.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
   $(".update").on("click", function() {
-    // console.log($(this).siblings(".title").val())
-  $("." + this.id).text($(this).siblings(".title").val())
+    $("." + this.id).text($(this).siblings(".title").val())
   })
 });
--- spec/features/user_can_login_spec.rb
+++ spec/features/user_can_login_spec.rb
@@ -15,4 +15,20 @@ RSpec.feature "user can login" do
 
     expect(current_path).to eq root_path
   end
+
+  scenario "user sees error with invalid login" do
+    User.create(username: "bob", password: "123", email: "b@gmail")
+
+    visit root_path
+
+    click_on "Log In"
+    within ".log-in" do
+      fill_in "Username", with: "jim"
+      fill_in "Password", with: "123"
+      click_on "Sign In"
+    end
+
+    expect(page).to have_content "Invalid Login"
+    expect(current_path).to eq authentication_path
+  end
 end
